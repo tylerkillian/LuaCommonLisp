92f7fbdca867f8bb5f626c1bd4ffc6263c8de254
Tyler Killian <tyler.killian@gmail.com>
2016-07-28 22:44:57 -0400

    Setting up framework

---
+++ main.lua
@@ -0,0 +1,14 @@
+Parser = {}
+
+function Parser:new()
+  local parser = {}
+  setmetatable(parser, self)
+  self.__index = self
+
+  return parser
+end
+
+function Parser:readCharacter(character)
+end
+
+local parser = Parser:new
