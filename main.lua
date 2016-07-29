48c2feb081fd4daf44a0e45ae443ed35d021b011
Tyler Killian <tyler.killian@gmail.com>
2016-07-28 23:08:14 -0400

    Setting up framework

--- main.lua
+++ main.lua
@@ -28,7 +28,9 @@ function Parser:readCharacter_scan(character)
 end
 
 function Parser:readCharacter_symbol(character)
-  if character == "(" then
+  if character == ")" then
+  end
+end
 
 function Parser:readCharacter(character)
   if self.mode == "scan" then
