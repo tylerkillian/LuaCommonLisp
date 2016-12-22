72bbba41e1e2774c1d90595605557e1342c0698c
Tyler Killian <tyler.killian@gmail.com>
2016-12-22 15:23:48 -0500

    setting up basic framework

--- main.lua
+++ main.lua
@@ -1,28 +1,6 @@
 require "Parser"
 require "Environment"
 
-local parser = Parser:new()
-local codeString = [[(format t (concatenate 'string "hel" "lo~%"))]]
-local code
-local environment = Environment:new()
-for codeStringIdx = 1, string.len(codeString) do
-  local nextCharacter = string.sub(codeString, codeStringIdx, codeStringIdx)
-  code = parser:readCharacter(nextCharacter)
-  if code then
-    environment:eval(code)
-  end
-end
-assert(type(code) == "table")
-assert(#code == 3)
-assert(code[1] == "format")
-assert(code[2] == "t")
-assert(type(code[3]) == "table")
-assert(#code[3] == 4)
-assert(code[3][1] == "concatenate")
-assert(code[3][2] == "'string")
-assert(code[3][3] == "\"hel\"")
-assert(code[3][4] == "\"lo~%\"")
-
 function codeToString(code)
   if type(code) == "string" then
     return code
