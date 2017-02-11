c66fc9c6392ee44ecc8fbc57f682af2edeb3acc9
Tyler Killian <tyler.killian@gmail.com>
2017-02-11 16:08:51 -0500

    setting up basic framework

--- CodeTypes.lua
+++ CodeTypes.lua
@@ -21,3 +21,15 @@ function Symbol:new(value)
 
   return symbol
 end
+
+Expression = {}
+
+function Expression:new()
+  local expression = {
+  }
+  setmetatable(expression, self)
+  self.__index = self
+
+  return expression
+end
+
