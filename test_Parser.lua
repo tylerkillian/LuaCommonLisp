579c688fa5e4f2835b08d1934150215523332f07
Tyler Killian <tyler.killian@gmail.com>
2017-02-18 22:56:01 -0500

    setting up basic framework

--- test_Parser.lua
+++ test_Parser.lua
@@ -9,52 +9,64 @@ end
 
 -- Begin unit tests
 
-local test_Parser = {}
+local test_Scanner = {}
 
-function test_Parser.Scanner_readSpace()
+function test_Scanner.readSpace()
   local scanner = Scanner:new()
   assert(not scanner:readCharacter(" "))
 end
 
-function test_Parser.SymbolReader_addSingleCharacter()
+local test_SymbolReader = {}
+
+function test_SymbolReader.addSingleCharacter()
   local symbolReader = SymbolReader:new()
   symbolReader:readCharacter("a")
   assert("a" == symbolReader:toString())
 end
 
-function test_Parser.SymbolReader_addTwoCharacters()
+function test_SymbolReader.addTwoCharacters()
   local symbolReader = SymbolReader:new()
   feedCharactersOneAtATime(symbolReader, "ab")
   assert("ab" == symbolReader:toString())
 end
 
-function test_Parser.SymbolReader_terminateWithSpace()
+function test_SymbolReader.terminateWithSpace()
   local symbolReader = SymbolReader:new()
   feedCharactersOneAtATime(symbolReader, "ab")
   assert("ab" == symbolReader:readCharacter(" "))
 end
 
-function test_Parser.SymbolReader_resetAfterTerminate()
+function test_SymbolReader.resetAfterTerminate()
   local symbolReader = SymbolReader:new()
   feedCharactersOneAtATime(symbolReader, "ab ")
   assert("" == symbolReader:toString())
 end
 
-function test_Parser.Parser_construct()
+local test_Parser = {}
+
+function test_Parser.construct()
   local defaultParser = Parser:new()
   assert("scan" == defaultParser:toString())
 end
 
-function test_Parser.Parser_switchFromScanToSymbol()
+function test_Parser.switchFromScanToSymbol()
   local parser = Parser:new()
   parser:readCharacter("a")
   assert("symbol" == parser:toString())
 end
 
-function testParser()
-  for testName, theTest in pairs(test_Parser) do
+-- End unit tests
+
+local function runTests(tests)
+  for testName, theTest in pairs(tests) do
     print("Running " .. testName)
     theTest()
   end
+end
+
+function testParser()
+  runTests(test_Scanner)
+  runTests(test_SymbolReader)
+  runTests(test_Parser)
 end
 
