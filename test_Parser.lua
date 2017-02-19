a1e4d1c3ca85e21da44eaaa41ba4370b00100336
Tyler Killian <tyler.killian@gmail.com>
2017-02-18 22:47:34 -0500

    setting up basic framework

--- test_Parser.lua
+++ test_Parser.lua
@@ -9,50 +9,50 @@ end
 
 -- Begin unit tests
 
-local Parser_tests = {}
+local test_Parser = {}
 
-function Parser_tests.Scanner_readSpace()
+function test_Parser.Scanner_readSpace()
   local scanner = Scanner:new()
   assert(not scanner:readCharacter(" "))
 end
 
-function Parser_tests.SymbolReader_addSingleCharacter()
+function test_Parser.SymbolReader_addSingleCharacter()
   local symbolReader = SymbolReader:new()
   symbolReader:readCharacter("a")
   assert("a" == symbolReader:toString())
 end
 
-function Parser_tests.SymbolReader_addTwoCharacters()
+function test_Parser.SymbolReader_addTwoCharacters()
   local symbolReader = SymbolReader:new()
   feedCharactersOneAtATime(symbolReader, "ab")
   assert("ab" == symbolReader:toString())
 end
 
-function Parser_tests.SymbolReader_terminateWithSpace()
+function test_Parser.SymbolReader_terminateWithSpace()
   local symbolReader = SymbolReader:new()
   feedCharactersOneAtATime(symbolReader, "ab")
   assert("ab" == symbolReader:readCharacter(" "))
 end
 
-function Parser_tests.SymbolReader_resetAfterTerminate()
+function test_Parser.SymbolReader_resetAfterTerminate()
   local symbolReader = SymbolReader:new()
   feedCharactersOneAtATime(symbolReader, "ab ")
   assert("" == symbolReader:toString())
 end
 
-function Parser_tests.Parser_construct()
+function test_Parser.Parser_construct()
   local defaultParser = Parser:new()
   assert("scan" == defaultParser:toString())
 end
 
-function Parser_tests.Parser_switchFromScanToSymbol()
+function test_Parser.Parser_switchFromScanToSymbol()
   local parser = Parser:new()
   parser:readCharacter("a")
   assert("symbol" == parser:toString())
 end
 
 function testParser()
-  for testName, theTest in pairs(Parser_tests) do
+  for testName, theTest in pairs(test_Parser) do
     print("Running " .. testName)
     theTest()
   end
