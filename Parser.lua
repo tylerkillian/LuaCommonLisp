d3192e429656ff361a0751bb94764ce2b6ab6f57
Tyler Killian <tyler.killian@gmail.com>
2017-02-19 17:28:20 -0500

    setting up basic framework

--- Parser.lua
+++ Parser.lua
@@ -151,6 +151,7 @@ function ExpressionReader:new(returnBy,name)
     returnBy = returnBy or "collection",
 name = name or "base",
   }
+print("new with " .. expressionReader.name)
   setmetatable(expressionReader, self)
   self.__index = self
 
