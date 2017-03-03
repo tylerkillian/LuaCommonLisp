ed73dea74ff3ec3ae105f019b62a24247fe66825
Tyler Killian <tyler.killian@gmail.com>
2017-03-03 12:15:25 -0500

    setting up basic framework

--- test_Parser.lua
+++ test_Parser.lua
@@ -203,7 +203,13 @@ local test_update_Scanner = {}
 function test_update_Scanner.readSpace()
   local scanner = update_Scanner:new()
   scanner:readCharacter(" ")
-  assert(not scanner:isTerminated(" "))
+  assert(not scanner:isTerminated())
+end
+
+function test_update_Scanner.readNonWhitespace()
+  local scanner = update_Scanner:new()
+  scanner:readCharacter("a")
+  assert(scanner:isTerminated())
 end
 
 -- End unit tests
