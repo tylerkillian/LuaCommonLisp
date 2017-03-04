a8a0b8ec4309e60223af8534de3e133fd31356a9
Tyler Killian <tyler.killian@gmail.com>
2017-03-04 14:53:00 -0500

    setting up basic framework

--- test_Parser.lua
+++ test_Parser.lua
@@ -197,10 +197,46 @@ function test_ExpressionReader.nested()
   assert("(a (b c))" == expressionReader:readCharacter(" "))
 end
 
+function test_ExpressionReader.nestedWithQuotes()
+  local expressionReader = ExpressionReader:new()
+  feedCharactersOneAtATime(expressionReader, "a '('b 'c))")
+  assert("(a '('b 'c))" == expressionReader:readCharacter(" "))
+end
+
 test_SingleQuoteReader = {}
 
 function test_SingleQuoteReader.startsWithSingleQuote()
   assert(SingleQuoteReader.startsWith("'"))
+end
+
+function test_SingleQuoteReader.quoteSymbol()
+  local quoteReader = SingleQuoteReader.startsWith("'")
+  quoteReader:readCharacter("a")
+  assert("'a" == quoteReader:readCharacter(" "))
+end
+
+function test_SingleQuoteReader.quoteString()
+  local quoteReader = SingleQuoteReader.startsWith("'")
+  feedCharactersOneAtATime(quoteReader, '"a"')
+  assert("'a" == quoteReader:readCharacter(" "))
+end
+
+function test_SingleQuoteReader.quoteExpression()
+  local quoteReader = SingleQuoteReader.startsWith("'")
+  feedCharactersOneAtATime(quoteReader, '("a" b)')
+  assert("'(a b)" == quoteReader:readCharacter(" "))
+end
+
+function test_SingleQuoteReader.quoteQuoteSymbol()
+  local quoteReader = SingleQuoteReader.startsWith("'")
+  feedCharactersOneAtATime(quoteReader, "'a")
+  assert("''a" == quoteReader:readCharacter(" "))
+end
+
+function test_ExpressionReader.nested()
+  local quoteReader = SingleQuoteReader.startsWith("'")
+  feedCharactersOneAtATime(quoteReader, "(a '(b 'c))")
+  assert("'(a '(b 'c))" == quoteReader:readCharacter(" "))
 end
 
 -- End unit tests
