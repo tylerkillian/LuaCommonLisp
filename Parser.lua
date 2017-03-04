a05caa822f153e314f742802ba697f9c2c56c6da
Tyler Killian <tyler.killian@gmail.com>
2017-03-04 10:46:38 -0500

    setting up basic framework

--- Parser.lua
+++ Parser.lua
@@ -372,3 +372,155 @@ end
 function update_SymbolReader:toString()
   return self.queue
 end
+
+ExpressionReader = {}
+
+function ExpressionReader:new(returnBy)
+  local expressionReader = {
+    nextLink = Scanner:new(),
+    operatorQueue = {},
+    state = "scan",
+    expression = {},
+    returnBy = returnBy or "collection",
+  }
+  setmetatable(expressionReader, self)
+  self.__index = self
+
+  return expressionReader
+end
+
+function ExpressionReader:reset()
+  self.nextLink = Scanner:new()
+  self.operatorQueue = {}
+  self.state = "scan"
+  self.expression = {}
+end
+
+local function getNewState(currentState, terminalCharacter)
+  if currentState == "scan" then
+
+    if terminalCharacter == "\"" then
+      return "string"
+    elseif terminalCharacter == "(" then
+      return "expression"
+    else
+      return "symbol"
+    end
+
+  elseif currentState == "symbol" then
+
+    if terminalCharacter == "\"" then
+      return "string"
+    elseif terminalCharacter == "(" then
+      return "expression"
+    else
+      return "scan"
+    end
+
+  elseif currentState == "string" then
+
+    if terminalCharacter == "\"" then
+      return "scan"
+    end
+
+  elseif currentState == "expression" then
+
+     if terminalCharacter == ")" then
+       return "scan"
+     end
+
+  end
+
+  assert(false)
+end
+
+local function isOperator()
+  return false
+end
+
+function ExpressionReader:changeState(currentStateTerminalCharacter)
+    self.state = getNewState(self.state, currentStateTerminalCharacter, self)
+
+    if self.state == "scan" then
+      self.nextLink = Scanner:new()
+    elseif self.state == "symbol" then
+      self.nextLink = SymbolReader:new()
+      self.nextLink:readCharacter(currentStateTerminalCharacter)
+    elseif self.state == "string" then
+      self.nextLink = StringReader:new()
+    elseif self.state == "expression" then
+      self.nextLink = ExpressionReader:new()
+    end
+end
+
+function ExpressionReader:callNextLink(character)
+  local linkResult = self.nextLink:readCharacter(character)
+
+  if linkResult and self.state ~= "scan" and self.returnBy == "collection" then
+    table.insert(self.expression, linkResult)
+  end
+
+  return linkResult
+end
+
+function ExpressionReader:returningExpression(character)
+  if self.returnBy == "collection" and self.state ~= "expression" and character == ")" then
+    return true
+  else
+    return false
+  end
+end
+
+function ExpressionReader:getReturnValue(linkResult, character)
+  if self.returnBy == "collection" then
+    if self:returningExpression(character) then
+      return self.expression
+    else
+      return
+    end
+  end
+
+  if not linkResult or self.state == "scan" then
+    return
+  end
+
+  return linkResult
+end
+
+function ExpressionReader:prepareForNextCharacter(linkResult, character)
+  if not linkResult then
+    return
+  end
+
+  if self:returningExpression(character) then
+    self:reset()
+  else
+    self:changeState(character)
+  end
+end
+
+function ExpressionReader:readCharacter(character)
+  local linkResult = self:callNextLink(character)
+
+  local result = self:getReturnValue(linkResult, character)
+
+  self:prepareForNextCharacter(linkResult, character)
+
+  return result
+end
+
+function ExpressionReader:toString()
+  if self.returnBy == "element" then
+    return self.state
+  end
+
+  if #self.expression == 0 then
+    return "():" .. self.state
+  end
+
+  local result = ""
+  for _, current in ipairs(self.expression) do
+    result = result .. " " .. current
+  end
+  return "(" .. string.sub(result, 2) .. "):" .. self.state
+end
