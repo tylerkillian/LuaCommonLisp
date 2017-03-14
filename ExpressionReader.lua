c17ac5cf689133564271a8d530dcd6bd84c89381
Tyler Killian <tyler.killian@gmail.com>
2017-03-14 19:36:43 -0400

    setting up basic framework

---
+++ ExpressionReader.lua
@@ -0,0 +1,88 @@
+Code = {
+  NULL = {},
+}
+
+ExpressionReader = {}
+
+function ExpressionReader.startsWith(character)
+  if character ~= "(" then
+    return
+  end
+
+  return ExpressionReader:new()
+end
+
+function ExpressionReader:new(readerFunctor)
+  local expressionReader = {
+    nextLink = nil,
+    expression = {},
+    isDone = false,
+    readerFunctor = readerFunctor or getNewReaderUsingInitialCharacter
+  }
+  setmetatable(expressionReader, self)
+  self.__index = self
+
+  return expressionReader
+end
+
+local function convertExpressionToString(expression)
+  if #expression == 0 then
+    return "()"
+  end
+
+  local result = ""
+  for _, current in ipairs(expression) do
+    result = result .. " " .. current
+  end
+  return "(" .. string.sub(result, 2) .. ")"
+end
+
+function ExpressionReader:readCharacter(character)
+  if not self.nextLink then
+    if character == ")" then
+      self.isDone = true
+    else
+      self.nextLink = self.readerFunctor(character)
+    end
+
+    return
+  end
+
+  if self.isDone then
+    return convertExpressionToString(self.expression)
+  end
+
+  local linkResult = self.nextLink:readCharacter(character)
+  if not linkResult then
+    return
+  end
+
+  if linkResult ~= Code.NULL then
+    table.insert(self.expression, linkResult)
+  end
+
+  if character == ")" then
+    self.isDone = true
+    return
+  end
+
+  self.nextLink = self.readerFunctor(character)
+end
+
+function ExpressionReader:toString()
+  local isDoneString = "false"
+  if self.isDone then
+    isDoneString = "true"
+  end
+
+  if #self.expression == 0 then
+    return "():" .. isDoneString
+  end
+
+  local result = ""
+  for _, current in ipairs(self.expression) do
+    result = result .. " " .. current
+  end
+  return "(" .. string.sub(result, 2) .. "):" .. isDoneString
+end
+
---
+++ test_ExpressionReader.lua
@@ -0,0 +1,122 @@
+require "Parser"
+
+local function feedCharactersOneAtATime(reader, characters)
+  for index = 1, string.len(characters) do
+    local nextCharacter = string.sub(characters, index, index)
+    reader:readCharacter(nextCharacter)
+  end
+end
+
+FakeReader = {}
+
+function FakeReader:new(returnValue)
+  local reader = {
+    returnValue = returnValue
+  }
+  setmetatable(reader, self)
+  self.__index = self
+
+  return reader
+end
+
+function FakeReader:readCharacter(character)
+  return self.returnValue
+end
+
+function createFakeReaderFunctor(readers)
+  local index = 0
+  return function(firstCharacter)
+      index = index + 1
+      return readers[index]
+  end
+end
+
+-- Begin unit tests
+
+local test_ExpressionReader = {}
+
+function test_ExpressionReader.startsWithParentheses()
+  assert(ExpressionReader.startsWith('('))
+end
+
+function test_ExpressionReader.doesNotStartWithAlphabeticCharacter()
+  assert(not ExpressionReader.startsWith('a'))
+end
+
+function test_ExpressionReader.doesNotStartWithNumber()
+  assert(not ExpressionReader.startsWith('1'))
+end
+
+function test_ExpressionReader.doesNotStartWithDoubleQuote()
+  assert(not ExpressionReader.startsWith('"'))
+end
+
+function test_ExpressionReader.construct()
+  local expressionReader = ExpressionReader:new()
+  assert("():false", expressionReader:toString())
+end
+
+function test_ExpressionReader.switchFromScanToSymbol()
+  local expressionReader = ExpressionReader:new()
+  expressionReader:readCharacter("a")
+  assert("():false" == expressionReader:toString())
+end
+
+function test_ExpressionReader.addSymbolToExpressionWhenReachSpace()
+  local expressionReader = ExpressionReader:new()
+  feedCharactersOneAtATime(expressionReader, "ab ")
+  assert("(ab):false" == expressionReader:toString())
+end
+
+function test_ExpressionReader.addSymbolToExpressionWhenReachString()
+  local expressionReader = ExpressionReader:new()
+  feedCharactersOneAtATime(expressionReader, 'ab"')
+  assert("(ab):false" == expressionReader:toString())
+end
+
+function test_ExpressionReader.startStringWhenReachInitialQuotationMark()
+  local expressionReader = ExpressionReader:new()
+  feedCharactersOneAtATime(expressionReader, '"')
+  assert("():false" == expressionReader:toString())
+end
+
+function test_ExpressionReader.returnStringWhenReachEndQuotationMark()
+  local expressionReader = ExpressionReader:new()
+  feedCharactersOneAtATime(expressionReader, '"ab" ')
+  assert("(ab):false" == expressionReader:toString())
+end
+
+function test_ExpressionReader.terminate()
+  local expressionReader = ExpressionReader:new(createFakeReaderFunctor{
+    FakeReader:new("ab"),
+    FakeReader:new("cd"),
+  })
+  feedCharactersOneAtATime(expressionReader, 'xx)')
+  assert("(ab cd)" == expressionReader:readCharacter(" "))
+end
+
+function test_ExpressionReader.nested()
+  local expressionReader = ExpressionReader:new(createFakeReaderFunctor{
+    FakeReader:new("a"),
+    ExpressionReader:new(createFakeReaderFunctor{
+      FakeReader:new("b"),
+      FakeReader:new("c"),
+    }),
+  })
+  feedCharactersOneAtATime(expressionReader, 'x(xx))')
+  assert("(a (b c))" == expressionReader:readCharacter(" "))
+end
+
+-- End unit tests
+
+local function runTests(testCategory, tests)
+  for testName, theTest in pairs(tests) do
+    print("Running " .. testCategory .. " " .. testName)
+    theTest()
+  end
+end
+
+function testParser()
+  runTests("ExpressionReader", test_ExpressionReader)
+end
+
