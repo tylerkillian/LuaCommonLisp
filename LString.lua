86e5045d344225202fc5c0da6f911d6346f241e0
Tyler Killian <tyler.killian@gmail.com>
2017-03-17 20:03:47 -0400

    setting up basic framework

---
+++ LString.lua
@@ -0,0 +1,56 @@
+LString = {}
+
+function LString:new(value)
+  local lString = {
+    data = value
+  }
+  setmetatable(lString, self)
+  self.__index = self
+
+  return lString
+end
+
+function LString:toString()
+  return self.data
+end
+
+Symbol = {}
+
+function Symbol:new(value)
+  local symbol = {
+    evaluate = true,
+    data = value or ""
+  }
+  setmetatable(symbol, self)
+  self.__index = self
+
+  return symbol
+end
+
+function Symbol:toString()
+  if self.evaluate then
+    return "true:" .. self.data
+  else
+    return "false:" .. self.data
+  end
+end
+
+Expression = {}
+
+function Expression:new()
+  local expression = {
+    evaluate = true,
+    data = {}
+  }
+  setmetatable(expression, self)
+  self.__index = self
+
+  return expression
+end
+
+function Expression:toString()
+  return "()"
+end
+
+TickOperator = {}
+
