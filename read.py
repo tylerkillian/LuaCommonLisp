60c17eb57e224ae1f1032f41141db576fb5f102b
Tyler Killian <tyler.killian@gmail.com>
2020-12-14 21:25:07 -0500

    .

--- read.py
+++ read.py
@@ -4,47 +4,42 @@ def _is_whitespace(x):
     return False
 
 def _is_macro_character(x):
-    if x == '(':
+    if x == '(' or x == ')':
         return True
     return False
 
 def _is_constituent_character(x):
+    if x in '+-':
+        return True
     if x >= 'a' and x  <= 'z':
         return True
     if x >= 'A' and x  <= 'Z':
         return True
     return False
 
-def _begin_read_s_expression(x, input_stream):
-    print(x)
+def _read_s_expression(x, input_stream):
     result = []
     next_token = None
     while not input_stream.at_eof():
         x = input_stream.read()
-        print(x)
         if x == ')':
             return result
         elif _is_whitespace(x):
             continue
         else:
             input_stream.unread(x)
-            read_result = read(input_stream)
-
-def _end_read_s_expression(x, input_stream):
-    input_stream.unread(')')
-    return True
+            next_token = read(input_stream)
+            if next_token:
+                result.append(next_token)
 
 def _get_macro_reader(x):
     if x == '(':
-        return _begin_read_s_expression
-    elif x == ')':
-        return _end_read_s_expression
+        return _read_s_expression
 
 def read(input_stream):
     token = ''
     while not input_stream.at_eof():
         x = input_stream.read()
-        print('read got ' + x)
         if _is_whitespace(x) and token == '':
             continue
         elif _is_macro_character(x) and token == '':
@@ -53,10 +48,11 @@ def read(input_stream):
             if macro_reader_result:
                 return macro_reader_result
             continue
+        elif _is_macro_character(x) and token != '':
+            input_stream.unread(x)
+            return token
         elif _is_constituent_character(x):
             token += x
-            print('token is now ' + token)
         else:
-            print('elsing')
             return token
     return
