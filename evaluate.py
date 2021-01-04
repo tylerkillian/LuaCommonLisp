8a3ac0607214c3e37585d6a9dcfcfd42931082ac
Tyler Killian <tyler.killian@gmail.com>
2021-01-03 20:23:43 -0500

    .

--- evaluate.py
+++ evaluate.py
@@ -1,2 +1,22 @@
+def is_symbol(form):
+    if form['type'] == 'symbol':
+        return True
+    else:
+        return False
+
+def is_symbol_macro(form):
+    return False
+
+def get_variable_value(environment, name):
+    return environment[name]
+
+def evaluate_symbol(environment, form):
+    if is_symbol_macro(form):
+        # not yet implemented
+        assert False
+    return get_variable_value(environment, form)
+
 def evaluate(environment, form):
-    pass
+    if is_symbol(form):
+        return evaluate_symbol(environment, form)
+
