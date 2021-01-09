0c5657e71eb37f5cf0505689a851230df1b912e0
Tyler Killian <tyler.killian@gmail.com>
2021-01-08 20:03:50 -0500

    .

--- standard_read_definition.py
+++ standard_read_definition.py
@@ -1,9 +1,13 @@
 import read
 import read_macro
+import symbol
 
 read_macro.read = read.read
 
+symbol.is_macro_character = read_macro.is_macro_character
+
 read.is_macro_character = read_macro.is_macro_character
 read.get_macro_reader = read_macro.get_macro_reader
+read.read_symbol = symbol.read_symbol
 
 read = read.read
--- symbol.py
+++ symbol.py
@@ -1,4 +1,7 @@
-is_
+from character import is_whitespace
+
+is_macro_character = None
+
 def Symbol(token):
     return {
         'form': 'symbol',
@@ -9,8 +12,8 @@ def read_symbol(x, input_stream):
     token = x
     while not input_stream.at_eof():
         x = input_stream.read()
-        if _is_whitespace(x) or _is_macro_character(x):
+        if is_whitespace(x) or is_macro_character(x):
             input_stream.unread(x)
             return Symbol(token)
         token += x
-    return result
+    return Symbol(token)
